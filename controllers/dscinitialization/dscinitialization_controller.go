0b14e40e707e023fe15456c6a36e42385242eb37
Edson Tirelli <etirelli@redhat.com>
2023-07-19 09:17:21 -0400

    Switching default value for component's enabled property to false

--- controllers/dscinitialization/dscinitialization_controller.go
+++ controllers/dscinitialization/dscinitialization_controller.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/retry"
 
+	"k8s.io/client-go/util/retry"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
